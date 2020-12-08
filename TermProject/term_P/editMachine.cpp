d7543a1b443dff504def963f14d7254cea8ba4dd
leecr1215 <leecr1215@naver.com>
2020-12-09 02:24:56 +0900

    move txt file

--- TermProject/term_P/editMachine.cpp
+++ TermProject/term_P/editMachine.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 
 using namespace std;
@@ -11,7 +12,17 @@ void main() {
 
 	ifstream in("test1.txt");
 
-	if()
+	if (in.is_open()) {
+		in >> input;
+		cout << input << endl;
+	}
+	else {
+		cout << "Can't find the file" << endl;
+	}
+
+	in.close();
+
+	
 
 
 }
