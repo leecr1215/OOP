250f792290a4be8201875b0e5bd0d46a42d8e6f6
leecr1215 <leecr1215@naver.com>
2020-12-09 02:15:05 +0900

    made term

---
+++ TermProject/term_P/editMachine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+void main() {
+	vector<string> vec;
+	string input, output;	// input에 txt 내용 다 저장
+
+	ifstream in("test1.txt");
+
+	if()
+
+
+}
